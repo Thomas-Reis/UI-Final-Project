539fa4705ee67c5444440e2f56bc7844e9e6ab29
GuyMcFail <34040408+GuyMcFail@users.noreply.github.com>
2018-11-23 23:44:00 -0500

    sdthgy
    hgf

--- Android/app/src/main/java/net/uoit/uifinalproject/home.java
+++ Android/app/src/main/java/net/uoit/uifinalproject/home.java
@@ -19,6 +19,8 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         newRoutineConfig();
+        addRoutine("Arm Day");
+        addRoutine("Leg Day");
     }
 
     void newRoutineConfig(){
@@ -33,46 +35,51 @@ public class home extends AppCompatActivity {
         });
     }
 
+    void addRoutine(String name){
+        LinearLayout rou_list = findViewById(R.id.routine_list);
+        final LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        final View routine = inflater.inflate(R.layout.routine_card, null);
+
+        TextView name_field =  routine.findViewById(R.id.routine_name);
+
+        name_field.setText(name);
+
+        routine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View expand_items = routine.findViewById(R.id.expand_items);
+                ImageView breakln = routine.findViewById(R.id.divider_line);
+                ImageView arrow = routine.findViewById(R.id.expand_arrow);
+
+                if (expand_items.getVisibility() == View.GONE){
+                    expand_items.setVisibility(View.VISIBLE);
+                    breakln.setVisibility(View.VISIBLE);
+                    arrow.setImageResource(android.R.drawable.arrow_up_float);
+                }
+                else {
+                    expand_items.setVisibility(View.GONE);
+                    breakln.setVisibility(View.GONE);
+                    arrow.setImageResource(android.R.drawable.arrow_down_float);
+                }
+            }
+        });
+
+        routine.setElevation(8);
+        rou_list.addView(routine, rou_list.getChildCount() - 1);
+
+    }
+
     @Override
     protected void onActivityResult(int request_code, int result_code, Intent data){
-        LinearLayout rou_list = findViewById(R.id.routine_list);
         if (request_code == 1){
             if(result_code == RESULT_CANCELED){
                 return;
             }
-            final LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            String name = data.getStringExtra("ROUTINE_NAME");
-
-            final View routine = inflater.inflate(R.layout.routine_card, null);
-
-            TextView name_field =  routine.findViewById(R.id.routine_name);
-
-            name_field.setText(name);
-
-            routine.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    View expand_items = routine.findViewById(R.id.expand_items);
-                    ImageView breakln = routine.findViewById(R.id.divider_line);
-                    ImageView arrow = routine.findViewById(R.id.expand_arrow);
-
-                    if (expand_items.getVisibility() == View.GONE){
-                        expand_items.setVisibility(View.VISIBLE);
-                        breakln.setVisibility(View.VISIBLE);
-                        arrow.setImageResource(android.R.drawable.arrow_up_float);
-                    }
-                    else {
-                        expand_items.setVisibility(View.GONE);
-                        breakln.setVisibility(View.GONE);
-                        arrow.setImageResource(android.R.drawable.arrow_down_float);
-                    }
-                }
-            });
-
-            routine.setPadding(0, 8, 0,0);
-            rou_list.addView(routine, rou_list.getChildCount() - 1);
+            else {
+                addRoutine(data.getStringExtra("ROUTINE_NAME"));
+            }
         }
     }
 }
