a09b1ed51e1e7f604ab2d40f56eae993a85c2132
Bradon-Lodwick <bradonlodwick22@gmail.com>
2018-11-28 03:44:30 -0500

    Completed question mark?

--- Android/app/src/main/java/net/uoit/uifinalproject/ViewRoutineActivity.java
+++ Android/app/src/main/java/net/uoit/uifinalproject/ViewRoutineActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +25,41 @@ public class ViewRoutineActivity extends AppCompatActivity {
         newExerciseConfig();
         heatmapStart();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubar_default, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile_option:
+                Intent profile_intent = new Intent(this, EditProfile.class);
+                startActivity(profile_intent);
+                return true;
+            case R.id.meals_option:
+                Intent meals_intent = new Intent(this, Food_Selection.class);
+                startActivity(meals_intent);
+                return true;
+            case R.id.calender_option:
+                Intent calendar_intent = new Intent(this, Calendar.class);
+                startActivity(calendar_intent);
+                return true;
+            case R.id.home_big_option:
+                Intent workouts_intent = new Intent(this, home.class);
+                //startActivity(workouts_intent);
+                finish();
+                return true;
+            case R.id.logout_option:
+                Intent login_intent = new Intent(this, LoginActivity.class);
+                startActivity(login_intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     void heatmapStart() {
         FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.haet_boi_FAB);
@@ -30,6 +68,14 @@ public class ViewRoutineActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent heat_intent = new Intent(ViewRoutineActivity.this, HeatyBoi.class);
                 startActivity(heat_intent);
+            }
+        });
+
+        Button log_button = findViewById(R.id.log_button);
+        log_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
