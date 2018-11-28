c701b2a89973105b6f3cc0f0a37f15f0403d7764
GuyMcFail <34040408+GuyMcFail@users.noreply.github.com>
2018-11-28 03:04:34 -0500

    sadf

--- Android/app/src/main/java/net/uoit/uifinalproject/HeatyBoi.java
+++ Android/app/src/main/java/net/uoit/uifinalproject/HeatyBoi.java
@@ -1,9 +1,11 @@
 package net.uoit.uifinalproject;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class HeatyBoi extends AppCompatActivity {
 
@@ -14,9 +16,19 @@ public class HeatyBoi extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("UIFinalProject");
         setSupportActionBar(toolbar);
+        goToExerciseConfig();
     }
 
-    void goToExercise(){
-        Intent intent = new Intent();
+    void goToExerciseConfig(){
+        FloatingActionButton FAB = findViewById(R.id.exercise_FAB);
+
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HeatyBoi.this, ViewRoutineActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
